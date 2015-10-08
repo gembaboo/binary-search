8049ecd4dc5a403302005c3f8665ae665003f394
Peter Harsfalvi <pharsfalvi@Peters-MacBook-Pro.local>
2015-10-08 20:56:38 +0200

    Second version after some TDD cycles

--- src/main/java/org/gembaboo/binarysearch/BinarySearch.java
+++ src/main/java/org/gembaboo/binarysearch/BinarySearch.java
@@ -1,16 +1,59 @@
 package org.gembaboo.binarysearch;
 
+import java.util.Arrays;
+
 public class BinarySearch {
 
-    // Finds the position of an element in an ordered array
-    public int getPosition(int element, int[] array) {
+    /**
+     * Finds the position of an value in an ordered array
+     *
+     * @param value the value which should be inserted
+     * @param array an integer array containing non-null ordered elements.
+     * @return the position of the value, where it should be inserted into the ordered array
+     * In case the value is smaller than the smallest element of the array, it returns -1. The same applies when the array is empty.
+     * In case the value is greater than the biggest element of the array, it returns array.length + 1.
+     *
+     * It throws IllegalArgumentException if the array is null, or it has null element, or its elements are not ordered.
+     */
+    public static int getPosition(int value, int[] array) {
 
+        validateParameters(array);
+        int lowIndex = 0;
+        int highIndex = array.length;
+        int position = -1;
+
+        while (lowIndex < highIndex) {
+
+            position = lowIndex + Math.round((float) (highIndex - lowIndex) / 2f) - 1;
+            int element = array[position];
+            if (position == 0 && value < element)
+                return -1;
+            else if (position == array.length - 1 && element < value)
+                return array.length;
+            else if (value < element)
+                highIndex = position;
+            else if (element < value)
+                lowIndex = position + 1;
+            else
+                return position;
+        }
+        return position;
+
+    }
+
+    private static void validateParameters(int[] array) {
         if (null == array) {
             throw new IllegalArgumentException("array can not be null");
         }
+        int[] sortedArray = Arrays.copyOf(array, array.length);
 
-        return -1;
+        Arrays.sort(sortedArray);
 
+        if (!Arrays.deepEquals(getObjectArray(sortedArray), getObjectArray(array)))
+            throw new IllegalArgumentException("array is not ordered");
+    }
 
+    private static Object[] getObjectArray(int[] array) {
+        return Arrays.stream(array).mapToObj(value -> value).toArray();
     }
 }
--- src/test/java/org/gembaboo/binarysearch/BinarySearchTest.java
+++ src/test/java/org/gembaboo/binarysearch/BinarySearchTest.java
@@ -1,33 +1,120 @@
 package org.gembaboo.binarysearch;
 
-import org.junit.Before;
 import org.junit.Test;
 
+import static org.gembaboo.binarysearch.BinarySearch.getPosition;
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
 
 public class BinarySearchTest {
 
 
-    private BinarySearch binarySearch;
-
-
     @Test(expected = IllegalArgumentException.class)
     public void WhenNullArray_ThrowsIllegalArgumentException() {
-        binarySearch.getPosition(0, null);
+        getPosition(0, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void WhenWrongOrderedArray_ThrowsIllegalArgumentException() {
+        getPosition(0, new int[]{3, 4, 6, 2, 5});
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void WhenArrayWithUninitializedMiddleElement_ThrowsIllegalArgumentException() {
+        int[] array = new int[5];
+        for (int i = 0; i < 5; i++) {
+            // for the third element keep it zero
+            if (i == 3)
+                continue;
+            array[i] = i;
+        }
+        getPosition(0, array);
     }
 
     @Test
-    public void WhenEmptyArray_ReturnsMinusOne() {
-
-        int position = binarySearch.getPosition(0, new int[0]);
-        assertSame(position, -1);
-
+    public void WhenEmptyArray_ReturnsMinus1() {
+        int position = getPosition(0, new int[0]);
+        assertSame(-1, position);
     }
 
 
-    @Before
-    public void setUp() {
-        binarySearch = new BinarySearch();
+    @Test
+    public void WithOneElementAndValueGreater_ReturnsLength() {
+        int position = getPosition(5, new int[]{2});
+        assertSame(1, position);
+    }
+
+    @Test
+    public void WithOneNegativeElementsAndValueGreater_ReturnsLength() {
+        int position = getPosition(5, new int[]{-2});
+        assertSame(1, position);
+    }
+
+    @Test
+    public void WithOneElementsAndValueSmaller_ReturnsMinus1() {
+        int position = getPosition(1, new int[]{2});
+        assertSame(-1, position);
+    }
+
+    @Test
+    public void WithOneNegativeElementAndValueSmaller_ReturnsMinus1() {
+        int position = getPosition(-3, new int[]{-2});
+        assertSame(-1, position);
+    }
+
+    @Test
+    public void WithTwoElementsAndValueSmaller_ReturnsMinus1() {
+        int position = getPosition(1, new int[]{2, 3});
+        assertSame(-1, position);
+    }
+
+
+    @Test
+    public void WithTwoNegativeElementsAndValueSmaller_ReturnsMinus1() {
+        int position = getPosition(-3, new int[]{-2, -1});
+        assertSame(-1, position);
+    }
+
+
+    @Test
+    public void WithTwoElementsAndValueGreater_ReturnsLength() {
+        int position = getPosition(5, new int[]{2, 3});
+        assertSame(2, position);
+    }
+
+
+    @Test
+    public void WithTwoNegativeElementsAndValueGreater_ReturnsLength() {
+        int position = getPosition(0, new int[]{-3, -2});
+        assertSame(2, position);
+    }
+
+    @Test
+    public void WithTwoElementsAndElementEqual_ReturnsEqualElementPosition1() {
+        int position = getPosition(3, new int[]{2, 3});
+        assertSame(1, position);
+    }
+
+
+    @Test
+    public void WithTwoElementsAndElementEqual_ReturnsEqualElementPosition2() {
+        int position = getPosition(2, new int[]{2, 3});
+        assertSame(0, position);
+    }
+
+
+    @Test
+    public void WithTwoElementsAndElementEqual_ReturnsEqualElementPosition3() {
+        int position = getPosition(4, new int[]{2, 3, 4, 5, 6});
+        assertSame(2, position);
+    }
+
+    @Test
+    public void WithTwoElementsAndElementEqual_ReturnsEqualElementPosition4() {
+        int position = getPosition(5, new int[]{2, 3, 4, 5, 5, 5, 6});
+        assertThat(position, anyOf(equalTo(3), equalTo(4), equalTo(5)));
     }
 
 
